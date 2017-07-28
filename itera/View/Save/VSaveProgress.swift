38a86ae105abb250e4cb63ff32be6cb27a87fb69
vauxhall <zero.griffin@gmail.com>
2017-07-28 03:08:59 -0500

    Layout view progress

--- itera/View/Save/VSaveProgress.swift
+++ itera/View/Save/VSaveProgress.swift
@@ -2,5 +2,41 @@ import UIKit
 
 class VSaveProgress:View<VSave, MSave, CSave>
 {
+    private weak var viewBar:VSaveProgressBar!
     
+    required init(controller:CSave)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        let viewBar:VSaveProgressBar = VSaveProgressBar(
+            controller:controller)
+        self.viewBar = viewBar
+        
+        let viewThumb:VSaveProgressThumb = VSaveProgressThumb(
+            controller:controller)
+        
+        addSubview(viewBar)
+        addSubview(viewThumb)
+        
+        NSLayoutConstraint.equals(
+            view:viewBar,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:viewThumb,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func updateProgress(percent:CGFloat)
+    {
+        viewBar.update(percent:percent)
+    }
 }
