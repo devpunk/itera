a3fc888dfb3dc09da0624c6f0cecd450d8f73db5
vauxhall <zero.griffin@gmail.com>
2017-07-29 06:22:32 -0500

    Layout image view

--- itera/View/EditRotate/VEditRotateImagePicture.swift
+++ itera/View/EditRotate/VEditRotateImagePicture.swift
@@ -3,5 +3,44 @@ import UIKit
 class VEditRotateImagePicture:
     View<VEditRotate, MEditRotate, CEditRotate>
 {
+    private let kMargin:CGFloat = 3
+    private let kBorderWidth:CGFloat = 1
     
+    required init(controller:CEditRotate)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        layer.borderWidth = kBorderWidth
+        layer.borderColor = UIColor.colourBackgroundGray.cgColor
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self,
+            margin:kMargin)
+        
+        guard
+        
+            let cgImage:CGImage = controller.model.edit.sequence?.items.first?.image
+        
+        else
+        {
+            return
+        }
+        
+        let image:UIImage = UIImage(cgImage:cgImage)
+        imageView.image = image
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
