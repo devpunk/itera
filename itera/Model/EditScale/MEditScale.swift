b2275851e81fe25b3eedb91d89f6d593a72c4af0
vauxhall <zero.griffin@gmail.com>
2017-07-29 02:50:48 -0500

    Reference original width and height

--- itera/Model/EditScale/MEditScale.swift
+++ itera/Model/EditScale/MEditScale.swift
@@ -5,11 +5,15 @@ class MEditScale:Model
     private(set) weak var edit:MEdit!
     var scaledWidth:CGFloat
     var scaledHeight:CGFloat
+    var originalWidth:CGFloat
+    var originalHeight:CGFloat
     
     required init()
     {
         scaledWidth = 0
         scaledHeight = 0
+        originalWidth = 0
+        originalHeight = 0
         
         super.init()
     }
@@ -19,5 +23,17 @@ class MEditScale:Model
     func config(edit:MEdit)
     {
         self.edit = edit
+        
+        guard
+        
+            let image:CGImage = edit.sequence?.items.first?.image
+        
+        else
+        {
+            return
+        }
+        
+        originalWidth = CGFloat(image.width)
+        originalHeight = CGFloat(image.height)
     }
 }
