62c1e150de40820da98ebb3b50e666b410e051e1
vauxhall <zero.griffin@gmail.com>
2017-07-28 19:58:53 -0500

    Factory views

--- itera/View/EditScale/VEditScaleImage.swift
+++ itera/View/EditScale/VEditScaleImage.swift
@@ -2,5 +2,23 @@ import UIKit
 
 class VEditScaleImage:View<VEditScale, MEditScale, CEditScale>
 {
+    weak var viewOriginal:VEditScaleImageOriginal!
+    weak var viewScaled:VEditScaleImageScaled!
+    let margin2:CGFloat
+    let kMargin:CGFloat = 30
     
+    required init(controller:CEditScale)
+    {
+        margin2 = kMargin + kMargin
+        
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        factoryViews()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- itera/View/EditScale/VEditScaleImageFactory.swift
+++ itera/View/EditScale/VEditScaleImageFactory.swift
@@ -2,5 +2,43 @@ import UIKit
 
 extension VEditScaleImage
 {
-    
+    func factoryViews()
+    {
+        let viewOriginal:VEditScaleImageOriginal = VEditScaleImageOriginal(
+            controller:controller)
+        self.viewOriginal = viewOriginal
+        
+        let viewScaled:VEditScaleImageScaled = VEditScaleImageScaled(
+            controller:controller)
+        self.viewScaled = viewScaled
+        
+        addSubview(viewOriginal)
+        addSubview(viewScaled)
+        
+        viewOriginal.layoutTop = NSLayoutConstraint.topToTop(
+            view:viewOriginal,
+            toView:self)
+        viewOriginal.layoutBottom = NSLayoutConstraint.bottomToBottom(
+            view:viewOriginal,
+            toView:self)
+        viewOriginal.layoutLeft = NSLayoutConstraint.leftToLeft(
+            view:viewOriginal,
+            toView:self)
+        viewOriginal.layoutRight = NSLayoutConstraint.rightToRight(
+            view:viewOriginal,
+            toView:self)
+        
+        viewScaled.layoutTop = NSLayoutConstraint.topToTop(
+            view:viewScaled,
+            toView:viewOriginal)
+        viewScaled.layoutBottom = NSLayoutConstraint.bottomToBottom(
+            view:viewScaled,
+            toView:viewOriginal)
+        viewScaled.layoutLeft = NSLayoutConstraint.leftToLeft(
+            view:viewScaled,
+            toView:viewOriginal)
+        viewScaled.layoutRight = NSLayoutConstraint.rightToRight(
+            view:viewScaled,
+            toView:viewOriginal)
+    }
 }
