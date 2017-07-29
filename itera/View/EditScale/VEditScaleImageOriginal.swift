9f0b99d1eb8eb3654fd6931d3cbf2021da5ffe9e
vauxhall <zero.griffin@gmail.com>
2017-07-28 19:54:35 -0500

    Create extension factory

--- itera/View/EditScale/VEditScaleImageOriginal.swift
+++ itera/View/EditScale/VEditScaleImageOriginal.swift
@@ -2,5 +2,8 @@ import UIKit
 
 class VEditScaleImageOriginal:View<VEditScale, MEditScale, CEditScale>
 {
-    
+    weak var layoutTop:NSLayoutConstraint!
+    weak var layoutBottom:NSLayoutConstraint!
+    weak var layoutLeft:NSLayoutConstraint!
+    weak var layoutRight:NSLayoutConstraint!
 }
--- itera/View/EditScale/VEditScaleImageScaled.swift
+++ itera/View/EditScale/VEditScaleImageScaled.swift
@@ -2,5 +2,8 @@ import UIKit
 
 class VEditScaleImageScaled:View<VEditScale, MEditScale, CEditScale>
 {
-    
+    weak var layoutTop:NSLayoutConstraint!
+    weak var layoutBottom:NSLayoutConstraint!
+    weak var layoutLeft:NSLayoutConstraint!
+    weak var layoutRight:NSLayoutConstraint!
 }
