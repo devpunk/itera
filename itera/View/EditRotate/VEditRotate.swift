8581ec948697912ef6be8b4268acedea274cc0ed
vauxhall <zero.griffin@gmail.com>
2017-07-29 05:31:09 -0500

    Layout view rotate

--- itera/View/EditRotate/VEditRotate.swift
+++ itera/View/EditRotate/VEditRotate.swift
@@ -2,5 +2,61 @@ import UIKit
 
 class VEditRotate:ViewMain
 {
+    private weak var layoutOkayLeft:NSLayoutConstraint!
+    private let kOkayWidth:CGFloat = 195
+    private let kOkayBottom:CGFloat = -20
+    private let kOkayHeight:CGFloat = 64
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+            
+            let controller:CEditRotate = controller as? CEditRotate
+            
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.width
+        let remainOkay:CGFloat = width - kOkayWidth
+        let okayMarginLeft:CGFloat = remainOkay / 2.0
+        layoutOkayLeft.constant = okayMarginLeft
+        
+        super.layoutSubviews()
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CEditRotate)
+    {
+        let viewOkay:VEditRotateOkay = VEditRotateOkay(
+            controller:controller)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:viewOkay,
+            toView:self,
+            constant:kOkayBottom)
+        NSLayoutConstraint.height(
+            view:viewOkay,
+            constant:kOkayHeight)
+        NSLayoutConstraint.width(
+            view:viewOkay,
+            constant:kOkayWidth)
+        layoutOkayLeft = NSLayoutConstraint.leftToLeft(
+            view:viewOkay,
+            toView:self)
+    }
 }
