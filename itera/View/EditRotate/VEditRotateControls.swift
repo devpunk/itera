6e328c7d4ef48e99acc77e6096418ca31277a305
vauxhall <zero.griffin@gmail.com>
2017-07-29 06:52:31 -0500

    Add rotate assets

--- itera/View/EditRotate/VEditRotateControls.swift
+++ itera/View/EditRotate/VEditRotateControls.swift
@@ -3,5 +3,15 @@ import UIKit
 class VEditRotateControls:
     View<VEditRotate, MEditRotate, CEditRotate>
 {
+    required init(controller:CEditRotate)
+    {
+        super.init(controller:controller)
+        
+        
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
