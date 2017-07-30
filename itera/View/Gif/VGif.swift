51cebc387d3e599b2533358996ebb119b98e6ce8
vauxhall <zero.griffin@gmail.com>
2017-07-30 17:25:10 -0500

    Factory gif with url

--- itera/View/Gif/VGif.swift
+++ itera/View/Gif/VGif.swift
@@ -2,5 +2,16 @@ import UIKit
 
 class VGif:UIView
 {
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        isUserInteractionEnabled = false
+        translatesAutoresizingMaskIntoConstraints = false
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- itera/View/Gif/VGifLoader.swift
+++ itera/View/Gif/VGifLoader.swift
@@ -2,5 +2,44 @@ import UIKit
 
 extension VGif
 {
+    class func withURL(url:URL) -> VGif
+    {
+        let view:VGif = VGif()
+        
+        return view
+    }
     
+    //MARK: private
+    
+    private func loadWithURL(url:URL)
+    {
+        guard
+        
+            let data:Data = loadData(url:url)
+        
+        else
+        {
+            return
+        }
+        
+        
+    }
+    
+    private func loadData(url:URL) -> Data?
+    {
+        let data:Data
+        
+        do
+        {
+            try data = Data(
+                contentsOf:url,
+                options:Data.ReadingOptions.uncached)
+        }
+        catch
+        {
+            return nil
+        }
+        
+        return data
+    }
 }
