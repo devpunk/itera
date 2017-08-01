7468e1bb46fe43966ec96ee5c99111049d15d9ce
vauxhall <zero.griffin@gmail.com>
2017-08-01 15:46:54 -0500

    Config model fullScreen

--- itera/Controller/FullScreen/CFullScreen.swift
+++ itera/Controller/FullScreen/CFullScreen.swift
@@ -2,5 +2,14 @@ import Foundation
 
 class CFullScreen:Controller<VFullScreen, MFullScreen>
 {
+    init(item:MHomeItem)
+    {
+        super.init()
+        model.config(item:item)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- itera/Model/FullScreen/MFullScreen.swift
+++ itera/Model/FullScreen/MFullScreen.swift
@@ -2,5 +2,12 @@ import Foundation
 
 class MFullScreen:Model
 {
+    private(set) weak var item:MHomeItem!
     
+    //MARK: public
+    
+    func config(item:MHomeItem)
+    {
+        self.item = item
+    }
 }
