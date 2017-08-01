ffd2dd8c0329e3fe48d3c3f8500cd4c6b53636cd
vauxhall <zero.griffin@gmail.com>
2017-08-01 16:48:54 -0500

    Layout menu

--- itera/View/FullScreen/VFullScreen.swift
+++ itera/View/FullScreen/VFullScreen.swift
@@ -2,5 +2,46 @@ import UIKit
 
 class VFullScreen:ViewMain
 {
+    private let kMenuHeight:CGFloat = 70
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CFullScreen = controller as? CFullScreen
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CFullScreen)
+    {
+        let viewMenu:VFullScreenMenu = VFullScreenMenu(
+            controller:controller)
+        
+        addSubview(viewMenu)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:viewMenu,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewMenu,
+            constant:kMenuHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewMenu,
+            toView:self)
+    }
 }
--- itera/View/FullScreen/VFullScreenMenu.swift
+++ itera/View/FullScreen/VFullScreenMenu.swift
@@ -3,7 +3,25 @@ import UIKit
 class VFullScreenMenu:VCollection<
     VFullScreen,
     MFullScreen,
-    CFullScreen>
+    CFullScreen,
+    VFullScreenMenuCell>
 {
+    private let kBorderWidth:CGFloat = 1
     
+    required init(controller:CFullScreen)
+    {
+        super.init(controller:controller)
+        
+        let border:VBorder = VBorder(colour:UIColor.colourBackgroundDark)
+        
+        addSubview(border)
+        
+        NSLayoutConstraint.topToTop(
+            view:border,toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
