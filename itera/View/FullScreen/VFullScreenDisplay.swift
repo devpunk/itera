cab8d7630c472072a432206734f288e4dce9a560
vauxhall <zero.griffin@gmail.com>
2017-08-01 18:18:54 -0500

    Add gif to display

--- itera/View/FullScreen/VFullScreenDisplay.swift
+++ itera/View/FullScreen/VFullScreenDisplay.swift
@@ -1,6 +1,26 @@
-import Foundation
+import UIKit
 
 class VFullScreenDisplay:View<VFullScreen, MFullScreen, CFullScreen>
 {
+    private weak var viewGif:VGif!
     
+    required init(controller:CFullScreen)
+    {
+        super.init(controller:controller)
+        
+        let path:URL = controller.model.item.path
+        let viewGif:VGif = VGif.withURL(url:path)
+        self.viewGif = viewGif
+        
+        addSubview(viewGif)
+        
+        NSLayoutConstraint.equals(
+            view:viewGif,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
