60aa650b309026f91b1b0dfa22d94422faa25eeb
vauxhall <zero.griffin@gmail.com>
2017-08-01 13:34:42 -0500

    Create item full screen

---
+++ itera/Model/Home/MHomeMenuFullScreen.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class MHomeMenuFullScreen:MHomeMenuProtocol
+{
+    var icon:UIImage
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetGenericFullScreen")
+        }
+    }
+    
+    func selected(controller:CHome)
+    {
+        
+    }
+}
