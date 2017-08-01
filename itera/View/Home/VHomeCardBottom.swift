cb48529c20460a8ffc89ef8b2f0e7ff399670df4
vauxhall <zero.griffin@gmail.com>
2017-08-01 08:01:20 -0500

    Add base to bottom

--- itera/View/Home/VHomeCardBottom.swift
+++ itera/View/Home/VHomeCardBottom.swift
@@ -1,6 +1,42 @@
-import Foundation
+import UIKit
 
 class VHomeCardBottom:View<VHome, MHome, CHome>
 {
+    private let kCornerRadius:CGFloat = 4
+    private let kBaseTop:CGFloat = 10
+    private let kBaseBottom:CGFloat = 10
+    private let kBaseMarginHorizontal:CGFloat = -2
+    private let kBorderWidth:CGFloat = 1
     
+    required init(controller:CHome)
+    {
+        super.init(controller:controller)
+        layer.cornerRadius = kCornerRadius
+        
+        let viewBase:UIView = UIView()
+        viewBase.isUserInteractionEnabled = false
+        viewBase.translatesAutoresizingMaskIntoConstraints = false
+        viewBase.clipsToBounds = true
+        viewBase.backgroundColor = UIColor.white
+        
+        addSubview(viewBase)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBase,
+            toView:self,
+            constant:kBaseTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:viewBase,
+            toView:self,
+            constant:kBaseBottom)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBase,
+            toView:self,
+            margin:kBaseMarginHorizontal)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
