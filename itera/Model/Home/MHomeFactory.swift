b6f6a840a142c9093d2d0e3f67cac951db4fa4ad
vauxhall <zero.griffin@gmail.com>
2017-08-01 12:20:44 -0500

    Create factory menu

--- itera/Model/Home/MHomeFactory.swift
+++ itera/Model/Home/MHomeFactory.swift
@@ -2,5 +2,13 @@ import Foundation
 
 extension MHome
 {
-    
+    class func factoryMenu() -> [MHomeMenuProtocol]
+    {
+        let itemShare:MHomeMenuShare = MHomeMenuShare()
+        
+        let items:[MHomeMenuProtocol] = [
+            itemShare]
+        
+        return items
+    }
 }
