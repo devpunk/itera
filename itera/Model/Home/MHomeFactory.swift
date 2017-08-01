79c090bf1d5840b87c7d0afb2bba764328293aa2
vauxhall <zero.griffin@gmail.com>
2017-08-01 13:43:30 -0500

    Create item delete

--- itera/Model/Home/MHomeFactory.swift
+++ itera/Model/Home/MHomeFactory.swift
@@ -5,9 +5,11 @@ extension MHome
     class func factoryMenu() -> [MHomeMenuProtocol]
     {
         let itemShare:MHomeMenuShare = MHomeMenuShare()
+        let itemFullScreen:MHomeMenuFullScreen = MHomeMenuFullScreen()
         
         let items:[MHomeMenuProtocol] = [
-            itemShare]
+            itemShare,
+            itemFullScreen]
         
         return items
     }
---
+++ itera/Model/Home/MHomeMenuDelete.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class MHomeMenuDelete:MHomeMenuProtocol
+{
+    var icon:UIImage
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetGenericDelete")
+        }
+    }
+    
+    func selected(controller:CHome)
+    {
+        
+    }
+}
