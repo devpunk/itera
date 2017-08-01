d5ffaf9266c050e030fb94127ae4cdd3b9f72670
vauxhall <zero.griffin@gmail.com>
2017-08-01 12:19:39 -0500

    Create item share

---
+++ itera/Model/Home/MHomeMenuShare.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class MHomeMenuShare:MHomeMenuProtocol
+{
+    var icon:UIImage
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetGenericShare")
+        }
+    }
+    
+    func selected(controller:CHome)
+    {
+
+    }
+}
