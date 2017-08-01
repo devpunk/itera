27aaeaacc6f8677c98b6161bbfd78908dfcb7c18
vauxhall <zero.griffin@gmail.com>
2017-07-31 20:33:42 -0500

    Enable paging

--- itera/View/Home/VHomeProjects.swift
+++ itera/View/Home/VHomeProjects.swift
@@ -21,6 +21,7 @@ class VHomeProjects:VCollection<
         
         super.init(controller:controller)
         collectionView.alwaysBounceHorizontal = true
+        collectionView.isPagingEnabled = true
         
         if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
         {
