d1c5ec960b1f4600c9e7bb7077ef346cfaa06ab8
vauxhall <zero.griffin@gmail.com>
2017-08-01 01:39:53 -0500

    Layout display

--- itera/View/Home/VHomeCard.swift
+++ itera/View/Home/VHomeCard.swift
@@ -4,7 +4,11 @@ class VHomeCard:View<VHome, MHome, CHome>
 {
     static let kWidth:CGFloat = 214
     weak var layoutTop:NSLayoutConstraint!
+    private weak var viewDisplay:VHomeCardDisplay!
     let kHeight:CGFloat = 320
+    private let kDisplayHeight:CGFloat = 260
+    private let kDisplayTop:CGFloat = 12
+    private let kDisplayMarginHorizontal:CGFloat = 9
     
     required init(controller:CHome)
     {
@@ -18,11 +22,28 @@ class VHomeCard:View<VHome, MHome, CHome>
         background.contentMode = UIViewContentMode.center
         background.image = #imageLiteral(resourceName: "assetGenericCard")
         
+        let viewDisplay:VHomeCardDisplay = VHomeCardDisplay(
+            controller:controller)
+        self.viewDisplay = viewDisplay
+        
         addSubview(background)
+        addSubview(viewDisplay)
         
         NSLayoutConstraint.equals(
             view:background,
             toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewDisplay,
+            toView:self,
+            constant:kDisplayTop)
+        NSLayoutConstraint.height(
+            view:viewDisplay,
+            constant:kDisplayHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewDisplay,
+            toView:self,
+            margin:kDisplayMarginHorizontal)
     }
     
     required init?(coder:NSCoder)
--- itera/View/Home/VHomeCardDisplay.swift
+++ itera/View/Home/VHomeCardDisplay.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class VHomeCardDisplay:View<VHome, MHome, CHome>
 {
+    private let kCornerRadius:CGFloat = 5
     
+    required init(controller:CHome)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        layer.cornerRadius = kCornerRadius
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
