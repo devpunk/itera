b81ca22cfec3d17779094470a2da949b9e07f6a8
vauxhall <zero.griffin@gmail.com>
2017-08-01 20:23:39 -0500

    Factory full screen menu

--- itera/Model/FullScreen/MFullScreen.swift
+++ itera/Model/FullScreen/MFullScreen.swift
@@ -2,7 +2,14 @@ import Foundation
 
 class MFullScreen:Model
 {
+    let menu:[MFullScreenProtocol]
     private(set) weak var item:MHomeItem!
+    
+    required init()
+    {
+        menu = MFullScreen.factoryMenu()
+        super.init()
+    }
     
     //MARK: public
     
--- itera/Model/FullScreen/MFullScreenFactory.swift
+++ itera/Model/FullScreen/MFullScreenFactory.swift
@@ -2,4 +2,13 @@ import Foundation
 
 extension MFullScreen
 {
+    class func factoryMenu() -> [MFullScreenProtocol]
+    {
+        let itemExitFullScreen:MFullScreenExitFullScreen = MFullScreenExitFullScreen()
+        
+        let items:[MFullScreenProtocol] = [
+            itemExitFullScreen]
+        
+        return items
+    }
 }
