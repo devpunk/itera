d20f7d76b5ee68026adf23398ea0ba954dcad7a8
vauxhall <zero.griffin@gmail.com>
2017-08-01 20:55:19 -0500

    Layout menu cell

--- itera/View/FullScreen/VFullScreenMenu.swift
+++ itera/View/FullScreen/VFullScreenMenu.swift
@@ -6,7 +6,9 @@ class VFullScreenMenu:VCollection<
     CFullScreen,
     VFullScreenMenuCell>
 {
+    private var cellSize:CGSize?
     private let kBorderHeight:CGFloat = 1
+    private let kCellWidth:CGFloat = 50
     
     required init(controller:CFullScreen)
     {
@@ -44,5 +46,73 @@ class VFullScreenMenu:VCollection<
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        insetForSectionAt section:Int) -> UIEdgeInsets
+    {
+        let width:CGFloat = collectionView.bounds.width
+        let countCells:CGFloat = CGFloat(controller.model.menu.count)
+        let cellsWidth:CGFloat = countCells * kCellWidth
+        let remainWidth:CGFloat = width - cellsWidth
+        let marginHorizontal:CGFloat = remainWidth / 2.0
+        let insets:UIEdgeInsets = UIEdgeInsets(
+            top:0,
+            left:marginHorizontal,
+            bottom:0,
+            right:marginHorizontal)
+        
+        return insets
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let height:CGFloat = collectionView.bounds.height
+            let cellSize:CGSize = CGSize(width:kCellWidth, height:height)
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.menu.count
+        
+        return count
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let item:MFullScreenProtocol = modelAtIndex(index:indexPath)
+        let cell:VFullScreenMenuCell = cellAtIndex(indexPath:indexPath)
+        
+        return cell
+    }
+    
+    //MARK: private
+    
+    private func modelAtIndex(index:IndexPath) -> MFullScreenProtocol
+    {
+        let item:MFullScreenProtocol = controller.model.menu[index.item]
+        
+        return item
     }
 }
--- itera/View/FullScreen/VFullScreenMenuCell.swift
+++ itera/View/FullScreen/VFullScreenMenuCell.swift
@@ -2,5 +2,37 @@ import UIKit
 
 class VFullScreenMenuCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config(model:MFullScreenProtocol)
+    {
+        imageView.image = model.icon
+    }
 }
