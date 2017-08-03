cae5d5420f0f9e350e4549c5b1d93f6c6bcc0ad4
vauxhall <zero.griffin@gmail.com>
2017-08-03 05:00:58 -0500

    Add cocoa pods

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+use_frameworks!
+
+def podlist
+end
+
+target 'itera' do
+  podlist
+end
+
+
---
+++ Pods/Target Support Files/Pods-itera/Pods-itera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_itera : NSObject
+@end
+@implementation PodsDummy_Pods_itera
+@end
