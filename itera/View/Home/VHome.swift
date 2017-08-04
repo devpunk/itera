ea9d1d09372b3c2dac0234e9f7a1ddc00a2c8f5a
vauxhall <zero.griffin@gmail.com>
2017-08-04 17:21:08 -0500

    Move animations to extension

--- itera/View/Home/VHome.swift
+++ itera/View/Home/VHome.swift
@@ -26,75 +26,6 @@ class VHome:ViewMain
         return nil
     }
     
-    //MARK: private
-    
-    private func displayNewCard()
-    {
-        guard
-            
-            let viewCard:VHomeCard = factoryCard()
-            
-        else
-        {
-            return
-        }
-        
-        layoutIfNeeded()
-        animateCardChangeFirst(viewCard:viewCard)
-    }
-    
-    private func animateCardChangeFirst(viewCard:VHomeCard)
-    {
-        self.viewCard?.layoutTop.constant = kCardMidMaxTop
-        viewCard.layoutTop.constant = kCardMinTop
-        
-        UIView.animate(
-            withDuration:kAnimationDuration,
-       animations:
-        { [weak self] in
-            
-            viewCard.alpha = 1
-            self?.viewCard?.alpha = 0
-            self?.layoutIfNeeded()
-        })
-        { [weak self] (done:Bool) in
-            
-            self?.animateCardChangeSecond(viewCard:viewCard)
-        }
-    }
-    
-    private func animateCardChangeSecond(viewCard:VHomeCard)
-    {
-        self.viewCard?.removeFromSuperview()
-        self.viewCard = viewCard
-        viewCard.layoutTop.constant = kCardMidMinTop
-        
-        UIView.animate(
-            withDuration:kAnimationFastDuration)
-        { [weak self] in
-            
-            self?.layoutIfNeeded()
-        }
-    }
-    
-    private func animateCardDisappear()
-    {
-        self.viewCard?.layoutTop.constant = kCardMidMaxTop
-        
-        UIView.animate(
-            withDuration:kAnimationDuration,
-            animations:
-        { [weak self] in
-            
-            self?.viewCard?.alpha = 0
-            self?.layoutIfNeeded()
-        })
-        { [weak self] (done:Bool) in
-            
-            self?.viewCard?.removeFromSuperview()
-        }
-    }
-    
     //MARK: public
     
     func viewWillDisappear()
@@ -105,26 +36,5 @@ class VHome:ViewMain
     func refresh()
     {
         viewProjects.refresh()
-    }
-    
-    func updateCard()
-    {
-        guard
-        
-            let controller:CHome = self.controller as? CHome
-        
-        else
-        {
-            return
-        }
-        
-        if let _:MHomeItem = controller.model.currentItem()
-        {
-            displayNewCard()
-        }
-        else
-        {
-            animateCardDisappear()
-        }
     }
 }
--- itera/View/Home/VHomeAnimations.swift
+++ itera/View/Home/VHomeAnimations.swift
@@ -2,5 +2,93 @@ import UIKit
 
 extension VHome
 {
+    func updateCard()
+    {
+        guard
+            
+            let controller:CHome = self.controller as? CHome
+            
+        else
+        {
+            return
+        }
+        
+        if let _:MHomeItem = controller.model.currentItem()
+        {
+            displayNewCard()
+        }
+        else
+        {
+            animateCardDisappear()
+        }
+    }
     
+    //MARK: private
+    
+    private func displayNewCard()
+    {
+        guard
+            
+            let viewCard:VHomeCard = factoryCard()
+            
+        else
+        {
+            return
+        }
+        
+        layoutIfNeeded()
+        animateCardChangeFirst(viewCard:viewCard)
+    }
+    
+    private func animateCardChangeFirst(viewCard:VHomeCard)
+    {
+        self.viewCard?.layoutTop.constant = kCardMidMaxTop
+        viewCard.layoutTop.constant = kCardMinTop
+        
+        UIView.animate(
+            withDuration:kAnimationDuration,
+            animations:
+            { [weak self] in
+                
+                viewCard.alpha = 1
+                self?.viewCard?.alpha = 0
+                self?.layoutIfNeeded()
+            })
+        { [weak self] (done:Bool) in
+            
+            self?.animateCardChangeSecond(viewCard:viewCard)
+        }
+    }
+    
+    private func animateCardChangeSecond(viewCard:VHomeCard)
+    {
+        self.viewCard?.removeFromSuperview()
+        self.viewCard = viewCard
+        viewCard.layoutTop.constant = kCardMidMinTop
+        
+        UIView.animate(
+            withDuration:kAnimationFastDuration)
+        { [weak self] in
+            
+            self?.layoutIfNeeded()
+        }
+    }
+    
+    private func animateCardDisappear()
+    {
+        self.viewCard?.layoutTop.constant = kCardMidMaxTop
+        
+        UIView.animate(
+            withDuration:kAnimationDuration,
+            animations:
+            { [weak self] in
+                
+                self?.viewCard?.alpha = 0
+                self?.layoutIfNeeded()
+            })
+        { [weak self] (done:Bool) in
+            
+            self?.viewCard?.removeFromSuperview()
+        }
+    }
 }
