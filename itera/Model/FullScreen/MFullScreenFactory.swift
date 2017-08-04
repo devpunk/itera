837dfd32c161c566e6a65d0ad356be71bb4bf7f6
vauxhall <zero.griffin@gmail.com>
2017-08-04 03:51:53 -0500

    Add share to factory

--- itera/Model/FullScreen/MFullScreenFactory.swift
+++ itera/Model/FullScreen/MFullScreenFactory.swift
@@ -4,9 +4,11 @@ extension MFullScreen
 {
     class func factoryMenu() -> [MFullScreenProtocol]
     {
+        let itemShare:MFullScreenShare = MFullScreenShare()
         let itemExitFullScreen:MFullScreenExitFullScreen = MFullScreenExitFullScreen()
         
         let items:[MFullScreenProtocol] = [
+            itemShare,
             itemExitFullScreen]
         
         return items
