108ee90e68e3b6816cdfb7192d4d4b29d01ac07f
vauxhall <zero.griffin@gmail.com>
2017-08-04 18:23:16 -0500

    Remove selection from cell

--- itera/View/Home/VHomeProjectsCell.swift
+++ itera/View/Home/VHomeProjectsCell.swift
@@ -4,17 +4,14 @@ class VHomeProjectsCell:UICollectionViewCell
 {
     static let kImageSize:CGFloat = 50
     private weak var imageView:UIImageView!
-    private weak var layoutImageLeft:NSLayoutConstraint!
     private let kBorderWidth:CGFloat = 1
-    private let kAlphaSelected:CGFloat = 0
-    private let kAlphaHighlighted:CGFloat = 0.2
-    private let kAlphaStand:CGFloat = 1
     
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        isUserInteractionEnabled = false
         
         let imageView:UIImageView = UIImageView()
         imageView.isUserInteractionEnabled = false
@@ -33,7 +30,7 @@ class VHomeProjectsCell:UICollectionViewCell
         NSLayoutConstraint.size(
             view:imageView,
             constant:VHomeProjectsCell.kImageSize)
-        layoutImageLeft = NSLayoutConstraint.leftToLeft(
+        NSLayoutConstraint.leftToLeft(
             view:imageView,
             toView:self)
     }
@@ -43,55 +40,10 @@ class VHomeProjectsCell:UICollectionViewCell
         return nil
     }
     
-    override func layoutSubviews()
-    {
-        let width:CGFloat = bounds.width
-        let remainWidth:CGFloat = width - VHomeProjectsCell.kImageSize
-        let marginLeft:CGFloat = remainWidth / 2.0
-        layoutImageLeft.constant = marginLeft
-        
-        super.layoutSubviews()
-    }
-    
-    override var isSelected:Bool
-    {
-        didSet
-        {
-            hover()
-        }
-    }
-    
-    override var isHighlighted:Bool
-    {
-        didSet
-        {
-            hover()
-        }
-    }
-    
-    //MARK: private
-    
-    private func hover()
-    {
-        if isSelected
-        {
-            imageView.alpha = kAlphaSelected
-        }
-        else if isHighlighted
-        {
-            imageView.alpha = kAlphaHighlighted
-        }
-        else
-        {
-            imageView.alpha = kAlphaStand
-        }
-    }
-    
     //MARK: public
     
     func config(model:MHomeItem)
     {
         imageView.image = model.image
-        hover()
     }
 }
