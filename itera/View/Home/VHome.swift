a099d9db31b4ea0ee8f80abea5611506306fa8ef
vauxhall <zero.griffin@gmail.com>
2017-08-04 17:14:54 -0500

    Move factories to extension

--- itera/View/Home/VHome.swift
+++ itera/View/Home/VHome.swift
@@ -2,33 +2,23 @@ import UIKit
 
 class VHome:ViewMain
 {
-    private(set) weak var viewProjects:VHomeProjects!
-    private(set) weak var viewCard:VHomeCard?
-    private var cardLeftMargin:CGFloat?
-    private let kProjectsHeight:CGFloat = 360
-    private let kGradientHeight:CGFloat = 250
-    private let kMenuHeight:CGFloat = 120
-    private let kCardMinTop:CGFloat = 40
-    private let kCardMidMinTop:CGFloat = 80
-    private let kCardMaxTop:CGFloat = 180
-    private let kCardMidMaxTop:CGFloat = 100
-    private let kAnimationDuration:TimeInterval = 0.3
-    private let kAnimationFastDuration:TimeInterval = 0.1
+    var cardLeftMargin:CGFloat?
+    weak var viewProjects:VHomeProjects!
+    weak var viewCard:VHomeCard?
+    let kProjectsHeight:CGFloat = 360
+    let kGradientHeight:CGFloat = 250
+    let kMenuHeight:CGFloat = 120
+    let kCardMinTop:CGFloat = 40
+    let kCardMidMinTop:CGFloat = 80
+    let kCardMaxTop:CGFloat = 180
+    let kCardMidMaxTop:CGFloat = 100
+    let kAnimationDuration:TimeInterval = 0.3
+    let kAnimationFastDuration:TimeInterval = 0.1
     
     required init(controller:UIViewController)
     {
         super.init(controller:controller)
-        
-        guard
-            
-            let controller:CHome = controller as? CHome
-        
-        else
-        {
-            return
-        }
-        
-        factoryViews(controller:controller)
+        factoryViews()
     }
     
     required init?(coder:NSCoder)
@@ -38,108 +28,11 @@ class VHome:ViewMain
     
     //MARK: private
     
-    private func factoryViews(controller:CHome)
-    {
-        let viewGradient:VGradient = VGradient.vertical(
-            colourTop:UIColor.colourGradientDark,
-            colourBottom:UIColor.colourGradientLight)
-        
-        let viewProjects:VHomeProjects = VHomeProjects(
-            controller:controller)
-        self.viewProjects = viewProjects
-        
-        let viewMenu:VHomeMenu = VHomeMenu(controller:controller)
-        
-        addSubview(viewGradient)
-        addSubview(viewProjects)
-        addSubview(viewMenu)
-        
-        NSLayoutConstraint.topToTop(
-            view:viewGradient,
-            toView:self)
-        NSLayoutConstraint.height(
-            view:viewGradient,
-            constant:kGradientHeight)
-        NSLayoutConstraint.equalsHorizontal(
-            view:viewGradient,
-            toView:self)
-        
-        NSLayoutConstraint.bottomToBottom(
-            view:viewMenu,
-            toView:self)
-        NSLayoutConstraint.height(
-            view:viewMenu,
-            constant:kMenuHeight)
-        NSLayoutConstraint.equalsHorizontal(
-            view:viewMenu,
-            toView:self)
-        
-        NSLayoutConstraint.topToTop(
-            view:viewProjects,
-            toView:self)
-        NSLayoutConstraint.height(
-            view:viewProjects,
-            constant:kProjectsHeight)
-        NSLayoutConstraint.equalsHorizontal(
-            view:viewProjects,
-            toView:self)
-    }
-    
-    //MARK: private
-    
-    private func newCard() -> VHomeCard?
-    {
-        guard
-            
-            let controller:CHome = self.controller as? CHome
-        
-        else
-        {
-            return nil
-        }
-        
-        let cardLeft:CGFloat
-        
-        if let cardLeftMargin:CGFloat = self.cardLeftMargin
-        {
-            cardLeft = cardLeftMargin
-        }
-        else
-        {
-            let width:CGFloat = bounds.width
-            let remainCard:CGFloat = width - VHomeCard.kWidth
-            cardLeft = remainCard / 2.0
-            self.cardLeftMargin = cardLeft
-        }
-        
-        let viewCard:VHomeCard = VHomeCard(
-            controller:controller)
-        
-        addSubview(viewCard)
-        
-        viewCard.layoutTop = NSLayoutConstraint.topToTop(
-            view:viewCard,
-            toView:self,
-            constant:kCardMaxTop)
-        NSLayoutConstraint.height(
-            view:viewCard,
-            constant:viewCard.kHeight)
-        NSLayoutConstraint.width(
-            view:viewCard,
-            constant:VHomeCard.kWidth)
-        NSLayoutConstraint.leftToLeft(
-            view:viewCard,
-            toView:self,
-            constant:cardLeft)
-        
-        return viewCard
-    }
-    
     private func displayNewCard()
     {
         guard
             
-            let viewCard:VHomeCard = newCard()
+            let viewCard:VHomeCard = factoryCard()
             
         else
         {
--- itera/View/Home/VHomeFactory.swift
+++ itera/View/Home/VHomeFactory.swift
@@ -2,5 +2,107 @@ import UIKit
 
 extension VHome
 {
+    func factoryViews()
+    {
+        guard
+            
+            let controller:CHome = controller as? CHome
+            
+        else
+        {
+            return
+        }
+        
+        let viewGradient:VGradient = VGradient.vertical(
+            colourTop:UIColor.colourGradientDark,
+            colourBottom:UIColor.colourGradientLight)
+        
+        let viewProjects:VHomeProjects = VHomeProjects(
+            controller:controller)
+        self.viewProjects = viewProjects
+        
+        let viewMenu:VHomeMenu = VHomeMenu(controller:controller)
+        
+        addSubview(viewGradient)
+        addSubview(viewProjects)
+        addSubview(viewMenu)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewGradient,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewGradient,
+            constant:kGradientHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewGradient,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:viewMenu,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewMenu,
+            constant:kMenuHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewMenu,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewProjects,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewProjects,
+            constant:kProjectsHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewProjects,
+            toView:self)
+    }
     
+    func factoryCard() -> VHomeCard?
+    {
+        guard
+            
+            let controller:CHome = self.controller as? CHome
+            
+        else
+        {
+            return nil
+        }
+        
+        let cardLeft:CGFloat
+        
+        if let cardLeftMargin:CGFloat = self.cardLeftMargin
+        {
+            cardLeft = cardLeftMargin
+        }
+        else
+        {
+            let width:CGFloat = bounds.width
+            let remainCard:CGFloat = width - VHomeCard.kWidth
+            cardLeft = remainCard / 2.0
+            self.cardLeftMargin = cardLeft
+        }
+        
+        let viewCard:VHomeCard = VHomeCard(
+            controller:controller)
+        
+        addSubview(viewCard)
+        
+        viewCard.layoutTop = NSLayoutConstraint.topToTop(
+            view:viewCard,
+            toView:self,
+            constant:kCardMaxTop)
+        NSLayoutConstraint.height(
+            view:viewCard,
+            constant:viewCard.kHeight)
+        NSLayoutConstraint.width(
+            view:viewCard,
+            constant:VHomeCard.kWidth)
+        NSLayoutConstraint.leftToLeft(
+            view:viewCard,
+            toView:self,
+            constant:cardLeft)
+        
+        return viewCard
+    }
 }
