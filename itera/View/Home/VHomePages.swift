803a2c7e35b13f5ce0cbc9e18094305cfd7806d5
vauxhall <zero.griffin@gmail.com>
2017-08-04 23:06:02 -0500

    Add selector to page control

--- itera/View/Home/VHomePages.swift
+++ itera/View/Home/VHomePages.swift
@@ -2,5 +2,55 @@ import UIKit
 
 class VHomePages:View<VHome, MHome, CHome>
 {
+    private weak var viewControl:UIPageControl!
     
+    required init(controller:CHome)
+    {
+        super.init(controller:controller)
+        
+        let viewControl:UIPageControl = UIPageControl()
+        viewControl.translatesAutoresizingMaskIntoConstraints = false
+        viewControl.addTarget(
+            self,
+            action:#selector(selectorValueChanged(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        addSubview(viewControl)
+        
+        NSLayoutConstraint.equals(
+            view:viewControl,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: selectors
+    
+    func selectorValueChanged(sender viewControl:UIPageControl)
+    {
+        controller.model.selected = viewControl.currentPage
+        
+        guard
+        
+            let view:VHome = controller.view as? VHome
+        
+        else
+        {
+            return
+        }
+        
+        view.upateSelection()
+    }
+    
+    //MARK: public
+    
+    func refresh()
+    {
+        let model:MHome = controller.model
+        viewControl.numberOfPages = model.items.count
+        viewControl.currentPage = model.selected
+    }
 }
