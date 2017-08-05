54248ef25a497aa348d3d525731bd51b3a573c6e
vauxhall <zero.griffin@gmail.com>
2017-08-05 09:13:22 -0500

    Create view progress

--- itera/View/SourceImageImport/VSourceImageImportProgress.swift
+++ itera/View/SourceImageImport/VSourceImageImportProgress.swift
@@ -1,9 +1,48 @@
-//
-//  VSourceImageImportProgress.swift
-//  itera
-//
-//  Created by zero on 8/5/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VSourceImageImportProgress:
+    View<VSourceImageImport, MSourceImageImport, CSourceImageImport>
+{
+    private weak var viewBar:VSourceImageImportProgressBar!
+    private weak var viewThumb:VSourceImageImportProgressThumb!
+    
+    required init(controller:CSourceImageImport)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        let viewBar:VSourceImageImportProgressBar = VSourceImageImportProgressBar(
+            controller:controller)
+        self.viewBar = viewBar
+        
+        let viewThumb:VSourceImageImportProgressThumb = VSourceImageImportProgressThumb(
+            controller:controller)
+        self.viewThumb = viewThumb
+        
+        addSubview(viewBar)
+        addSubview(viewThumb)
+        
+        NSLayoutConstraint.equals(
+            view:viewBar,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:viewThumb,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: internal
+    
+    func updateProgress(percent:CGFloat, image:CGImage)
+    {
+        let image:UIImage = UIImage(cgImage:image)
+        
+        viewBar.update(percent:percent)
+        viewThumb.imageView.image = image
+    }
+}
