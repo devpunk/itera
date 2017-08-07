16e6f8e02707798a208ab25927641446365b62e2
vauxhall <zero.griffin@gmail.com>
2017-08-07 09:17:10 -0500

    Add tutorial check

--- itera/Model/Home/MHomeTutorial.swift
+++ itera/Model/Home/MHomeTutorial.swift
@@ -1,9 +1,9 @@
-//
-//  MHomeTutorial.swift
-//  itera
-//
-//  Created by zero on 8/7/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+extension MHome
+{
+    func tutorialCheck(projects:[DProjectUser])
+    {
+        
+    }
+}
