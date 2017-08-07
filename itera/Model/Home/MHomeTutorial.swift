0d7b32b39da65341df117ff5afe309da6a90fa14
vauxhall <zero.griffin@gmail.com>
2017-08-07 09:51:52 -0500

    Load tutorials when

--- itera/Model/Home/MHomeTutorial.swift
+++ itera/Model/Home/MHomeTutorial.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CoreData
 
 extension MHome
 {
@@ -18,6 +19,41 @@ extension MHome
     
     private func addTutorial(projects:[DProjectUser])
     {
+        DManager.sharedInstance?.fetch(
+            entity:DProjectTutorial.self)
+        { [weak self] (data:[NSManagedObject]?) in
+            
+            guard
+            
+                let tutorials:[DProjectTutorial] = data as? [DProjectTutorial]
+            
+            else
+            {
+                return
+            }
+            
+            self?.merge(
+                projects:projects,
+                tutorials:tutorials)
+        }
+    }
+    
+    private func merge(
+        projects:[DProjectUser],
+        tutorials:[DProjectTutorial])
+    {
+        var merged:[DProject] = []
         
+        for tutorial:DProjectTutorial in tutorials
+        {
+            merged.append(tutorial)
+        }
+        
+        for project:DProject in projects
+        {
+            merged.append(project)
+        }
+        
+        projectsLoaded(projects:merged)
     }
 }
