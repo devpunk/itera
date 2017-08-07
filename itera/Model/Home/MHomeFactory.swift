10103a1ad434eea269d69f841329c55c269db0c5
vauxhall <zero.griffin@gmail.com>
2017-08-07 09:37:58 -0500

    Move factory tutorials to session

--- itera/Model/Home/MHomeFactory.swift
+++ itera/Model/Home/MHomeFactory.swift
@@ -1,10 +1,7 @@
 import Foundation
-import CoreData
 
 extension MHome
 {
-    private static let kAssetTutorialImages:String = "assetGifTutorial0.gif"
-    
     class func factoryMenu() -> [MHomeMenuProtocol]
     {
         let itemShare:MHomeMenuShare = MHomeMenuShare()
@@ -17,44 +14,5 @@ extension MHome
             itemDelete]
         
         return items
-    }
-    
-    class func factoryTutorials(
-        completion:@escaping(() -> ()))
-    {
-        factoryTutorialImages(completion:completion)
-    }
-    
-    //MARK: private
-    
-    private class func factoryTutorialImages(
-        completion:@escaping(() -> ()))
-    {
-        DManager.sharedInstance?.create(
-            entity:DProjectTutorial.self)
-        { (data:NSManagedObject?) in
-            
-            guard
-                
-                let project:DProjectTutorial = data as? DProjectTutorial
-                
-            else
-            {
-                return
-            }
-            
-            project.name = kAssetTutorialImages
-            
-            factoryTutorialsFinished(completion:completion)
-        }
-    }
-    
-    private class func factoryTutorialsFinished(
-        completion:@escaping(() -> ()))
-    {
-        DManager.sharedInstance?.save
-        {
-            completion()
-        }
     }
 }
--- itera/Model/Main/Session/MSessionTutorials.swift
+++ itera/Model/Main/Session/MSessionTutorials.swift
@@ -3,5 +3,44 @@ import CoreData
 
 extension MSession
 {
+    private static let kAssetTutorialImages:String = "assetGifTutorial0.gif"
     
+    class func factoryTutorials(
+        completion:@escaping(() -> ()))
+    {
+        factoryTutorialImages(completion:completion)
+    }
+    
+    //MARK: private
+    
+    private class func factoryTutorialImages(
+        completion:@escaping(() -> ()))
+    {
+        DManager.sharedInstance?.create(
+            entity:DProjectTutorial.self)
+        { (data:NSManagedObject?) in
+            
+            guard
+                
+                let project:DProjectTutorial = data as? DProjectTutorial
+                
+            else
+            {
+                return
+            }
+            
+            project.name = kAssetTutorialImages
+            
+            factoryTutorialsFinished(completion:completion)
+        }
+    }
+    
+    private class func factoryTutorialsFinished(
+        completion:@escaping(() -> ()))
+    {
+        DManager.sharedInstance?.save
+        {
+            completion()
+        }
+    }
 }
